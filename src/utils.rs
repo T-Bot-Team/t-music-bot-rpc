53eed6553318cfcb75bc50d59af2f8e975aa9b47
TehPig <aggelosdellas@gmail.com>
2026-06-09 01:21:55 +0300

    chore: release v2.0.3

--- src/utils.rs
+++ src/utils.rs
@@ -81,6 +81,12 @@ pub fn get_audio_devices() -> Vec<String> {
     if let Ok(devices) = host.output_devices() {
         for d in devices {
             if let Ok(name) = d.name() {
+                #[cfg(target_os = "linux")]
+                {
+                    if name.contains(':') || name.contains('=') {
+                        continue;
+                    }
+                }
                 if seen.insert(name.clone()) {
                     names.push(name);
                 }
